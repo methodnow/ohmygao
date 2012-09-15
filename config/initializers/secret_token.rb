ef6fa2942af45efd13c2fd76264736f2ea03fed7
Ernest Sim <ernest.sim@actatlys.com>
2012-09-15 10:10:13 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ohmygao::Application.config.secret_token = '4308ba3c5afeb04cce2007cd51435d9709defd4428ce0b7772b6bba69cc22c08dec27728d13486e78ba93918d83d93144284c6965574c0a6fa95c93200acea23'
